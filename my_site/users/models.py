f63e7986c0f38e6ee53086b48240f3df91b55f35
Sasha Osin <sashaosin364@gmail.com>
2023-03-17 22:01:27 +0200

    add product for catalog,fix user model and edit profile

--- my_site/users/models.py
+++ my_site/users/models.py
@@ -3,3 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 class Users(AbstractUser):
     phone = models.CharField(max_length=50,unique=True)
+    photo = models.ImageField(blank=True,upload_to='profile')
